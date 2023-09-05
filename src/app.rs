4514e01e641fb37d26735f715a7992384001e44d
zhouhang95 <765229842@qq.com>
2023-09-05 23:29:58 +0800

    page

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,27 @@
 use std::ffi::OsStr;
 
 use egui::{TextStyle, ScrollArea};
+use egui_extras::{Column, TableBuilder};
 
 use crate::{motion::Motion, pmm::read_pmm};
+
+#[derive(PartialEq)]
+enum Page {
+    Info,
+    Material,
+    Bone,
+    Morph,
+    Frame,
+    RigidBody,
+    Joint,
+    VmdBone,
+    VmdMorph,
+}
 
 pub struct TemplateApp {
     vmd_path: Option<std::path::PathBuf>,
     vmd_motion: Option<Motion>,
+    page: Page,
 
     bone_cur_value: usize,
     log_text: String,
@@ -19,6 +34,7 @@ impl Default for TemplateApp {
             vmd_motion: None,
             bone_cur_value: 0,
             log_text: String::new(),
+            page: Page::VmdBone,
         }
     }
 }
@@ -118,103 +134,124 @@ impl eframe::App for TemplateApp {
                     }
                 });
             });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            let mut bone_names = Vec::new();
-            let mut bone_keyframe_counts = Vec::new();
-            if let Some(m) = &self.vmd_motion {
-                ui.heading(&m.model_name);
-                for (bn, kfs) in m.get_bone_keyframes() {
-                    bone_names.push(bn);
-                    bone_keyframe_counts.push(kfs.len());
-                }
-            }
             ui.horizontal(|ui| {
-                let text = format!("Count: {}", bone_names.len());
-                ui.heading(text);
+                ui.selectable_value(&mut self.page, Page::Info, "Info");
+                ui.selectable_value(&mut self.page, Page::Material, "Material");
+                ui.selectable_value(&mut self.page, Page::Bone, "Bone");
+                ui.selectable_value(&mut self.page, Page::Morph, "Morph");
+                ui.selectable_value(&mut self.page, Page::Frame, "Frame");
+                ui.selectable_value(&mut self.page, Page::RigidBody, "RigidBody");
+                ui.selectable_value(&mut self.page, Page::Joint, "Joint");
+                ui.selectable_value(&mut self.page, Page::VmdBone, "VmdBone");
+                ui.selectable_value(&mut self.page, Page::VmdMorph, "VmdMorph");
             });
-            ui.separator();
-            let text_style = TextStyle::Body;
-            let row_height = ui.text_style_height(&text_style);
-            let num_rows = bone_names.len();
-            ScrollArea::vertical().auto_shrink([false; 2]).show_rows(
-                ui,
-                row_height,
-                num_rows,
-                |ui, row_range| {
-                    ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {                        
-                        for row in row_range {
-                            let text = format!("{:3}: {} ({})", row, bone_names[row], bone_keyframe_counts[row]);
-                            ui.selectable_value(&mut self.bone_cur_value, row, text);
-                        }
-                    });
-                },
-            );
         });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            use egui_extras::{Column, TableBuilder};
-            let mut bone_cur_keyframe = Vec::new();
-            let mut bone_names = Vec::new();
-            let mut bone_keyframe_counts = Vec::new();
-            if let Some(m) = &self.vmd_motion {
-                for (bn, kfs) in m.get_bone_keyframes() {
-                    bone_names.push(bn);
-                    bone_keyframe_counts.push(kfs.len());
+        egui::SidePanel::left("side_panel").show(ctx, |ui| match self.page {
+            Page::VmdBone => {
+                let mut bone_names = Vec::new();
+                let mut bone_keyframe_counts = Vec::new();
+                if let Some(m) = &self.vmd_motion {
+                    ui.heading(&m.model_name);
+                    for (bn, kfs) in m.get_bone_keyframes() {
+                        bone_names.push(bn);
+                        bone_keyframe_counts.push(kfs.len());
+                    }
                 }
-                if self.bone_cur_value < bone_names.len() {
-                    let bone_name = &bone_names[self.bone_cur_value];
-                    bone_cur_keyframe = m.get_bone_keyframes().get(bone_name).unwrap().clone();
-                }
-            }
-
-            let text_height = egui::TextStyle::Body.resolve(ui.style()).size;
-
-            let table = TableBuilder::new(ui)
-                .striped(true)
-                .resizable(true)
-                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                .column(Column::auto())
-                .column(Column::auto())
-                .column(Column::initial(100.0))
-                .column(Column::remainder())
-                .min_scrolled_height(0.0);
-
-            table
-                .header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.strong("Index");
-                    });
-                    header.col(|ui| {
-                        ui.strong("Frame");
-                    });
-                    header.col(|ui| {
-                        ui.strong("Translate");
-                    });
-                    header.col(|ui| {
-                        ui.strong("Rotation");
-                    });
-                })
-                .body(|body|  {
-                    body.rows(text_height, bone_cur_keyframe.len(), |row_index, mut row| {
-                        row.col(|ui| {
-                            ui.label(row_index.to_string());
-                        });
-                        row.col(|ui| {
-                            let frame = bone_cur_keyframe[row_index].frame;
-                            ui.label(frame.to_string());
-                        });
-                        row.col(|ui| {
-                            let trans = bone_cur_keyframe[row_index].trans;
-                            ui.label(format!("{:.2}, {:.2}, {:.2}", trans[0], trans[1], trans[2]));
-                        });
-                        row.col(|ui| {
-                            let rot = bone_cur_keyframe[row_index].rot;
-                            ui.label(format!("{}, {}, {}, {}", rot[0], rot[1], rot[2], rot[3]));
-                        });
-                    });
+                ui.horizontal(|ui| {
+                    let text = format!("Count: {}", bone_names.len());
+                    ui.heading(text);
                 });
+                ui.separator();
+                let text_style = TextStyle::Body;
+                let row_height = ui.text_style_height(&text_style);
+                let num_rows = bone_names.len();
+                ScrollArea::vertical().auto_shrink([false; 2]).show_rows(
+                    ui,
+                    row_height,
+                    num_rows,
+                    |ui, row_range| {
+                        ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {                        
+                            for row in row_range {
+                                let text = format!("{:3}: {} ({})", row, bone_names[row], bone_keyframe_counts[row]);
+                                ui.selectable_value(&mut self.bone_cur_value, row, text);
+                            }
+                        });
+                    },
+                );
+            },
+            Page::VmdMorph => {
+            },
+            _ => {},
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| match self.page {
+            Page::VmdBone => {
+                let mut bone_cur_keyframe = Vec::new();
+                let mut bone_names = Vec::new();
+                let mut bone_keyframe_counts = Vec::new();
+                if let Some(m) = &self.vmd_motion {
+                    for (bn, kfs) in m.get_bone_keyframes() {
+                        bone_names.push(bn);
+                        bone_keyframe_counts.push(kfs.len());
+                    }
+                    if self.bone_cur_value < bone_names.len() {
+                        let bone_name = &bone_names[self.bone_cur_value];
+                        bone_cur_keyframe = m.get_bone_keyframes().get(bone_name).unwrap().clone();
+                    }
+                }
+    
+                let text_height = egui::TextStyle::Body.resolve(ui.style()).size;
+    
+                let table = TableBuilder::new(ui)
+                    .striped(true)
+                    .resizable(true)
+                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                    .column(Column::auto())
+                    .column(Column::auto())
+                    .column(Column::initial(100.0))
+                    .column(Column::remainder())
+                    .min_scrolled_height(0.0);
+    
+                table
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.strong("Index");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Frame");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Translate");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Rotation");
+                        });
+                    })
+                    .body(|body|  {
+                        body.rows(text_height, bone_cur_keyframe.len(), |row_index, mut row| {
+                            row.col(|ui| {
+                                ui.label(row_index.to_string());
+                            });
+                            row.col(|ui| {
+                                let frame = bone_cur_keyframe[row_index].frame;
+                                ui.label(frame.to_string());
+                            });
+                            row.col(|ui| {
+                                let trans = bone_cur_keyframe[row_index].trans;
+                                ui.label(format!("{:.2}, {:.2}, {:.2}", trans[0], trans[1], trans[2]));
+                            });
+                            row.col(|ui| {
+                                let rot = bone_cur_keyframe[row_index].rot;
+                                ui.label(format!("{}, {}, {}, {}", rot[0], rot[1], rot[2], rot[3]));
+                            });
+                        });
+                    });
+            },
+            Page::VmdMorph => {
+
+            },
+            _ => {},
         });
 
         if false {
