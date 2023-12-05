6c2b4860d2db22ad06d9ccfc390194ecb91338c5
zhouhang95 <765229842@qq.com>
2023-12-05 11:31:52 +0800

    fix pmx write

--- src/pmx_writer.rs
+++ src/pmx_writer.rs
@@ -18,6 +18,44 @@ impl Pmx {
         file.write(bytemuck::cast_slice_mut(&mut string)).unwrap();
     }
 
+    fn get_int_size(s: usize) -> u8 {
+        if s <= 127 {
+            1
+        } else if s <= 32767 {
+            2
+        } else {
+            4
+        }
+    }
+
+    fn get_uint_size(s: usize) -> u8 {
+        if s <= 255 {
+            1
+        } else if s <= 65535 {
+            2
+        } else {
+            4
+        }
+    }
+
+    fn write_int(file: &mut Cursor<Vec<u8>>, v: i32, index_size: u8) {
+        match index_size {
+            1 => file.write_i8(v as _).unwrap(),
+            2 => file.write_i16::<LE>(v as _).unwrap(),
+            4 => file.write_i32::<LE>(v).unwrap(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn write_uint(file: &mut Cursor<Vec<u8>>, v: i32, index_size: u8) {
+        match index_size {
+            1 => file.write_u8(v as _).unwrap(),
+            2 => file.write_u16::<LE>(v as _).unwrap(),
+            4 => file.write_i32::<LE>(v).unwrap(),
+            _ => unreachable!(),
+        }
+    }
+
     pub fn write(&self) -> Vec<u8> {
         let content = Vec::new();
         let mut file = std::io::Cursor::new(content);
@@ -27,27 +65,40 @@ impl Pmx {
 
         file.write_u8(0).unwrap(); // use uft-16
         file.write_u8(0).unwrap(); // appendix_uv
-        file.write_u8(4).unwrap(); // vertex_index_size
-        file.write_u8(4).unwrap(); // texture_index_size
-        file.write_u8(4).unwrap(); // material_index_size
-        file.write_u8(4).unwrap(); // bone_index_size
-        file.write_u8(4).unwrap(); // morph_index_size
-        file.write_u8(4).unwrap(); // rigidbody_index_size
+        let vertex_index_size = Pmx::get_uint_size(self.verts.len());
+        let texture_index_size = Pmx::get_int_size(self.texs.len());
+        let material_index_size = Pmx::get_int_size(self.mats.len());
+        let bone_index_size = Pmx::get_int_size(self.bones.len());
+        let morph_index_size = Pmx::get_int_size(self.morphs.len());
+        let rigidbody_index_size = Pmx::get_int_size(self.rigidbodys.len());
+        file.write_u8(vertex_index_size).unwrap();
+        file.write_u8(texture_index_size).unwrap();
+        file.write_u8(material_index_size).unwrap();
+        file.write_u8(bone_index_size).unwrap();
+        file.write_u8(morph_index_size).unwrap();
+        file.write_u8(rigidbody_index_size).unwrap();
 
         Self::write_string(&mut file, &self.name);
         Self::write_string(&mut file, &self.name_en);
         Self::write_string(&mut file, &self.comment);
         Self::write_string(&mut file, &self.comment_en);
         
-        self.write_verts(&mut file);
-        self.write_faces(&mut file);
+        self.write_verts(&mut file, bone_index_size);
+        self.write_faces(&mut file, vertex_index_size);
         self.write_texs(&mut file);
-        self.write_mats(&mut file);
-        self.write_bones(&mut file);
-        self.write_morphs(&mut file);
+        self.write_mats(&mut file, texture_index_size);
+        self.write_bones(&mut file, bone_index_size);
+        self.write_morphs(
+            &mut file,
+            vertex_index_size,
+            material_index_size,
+            bone_index_size,
+            morph_index_size,
+            rigidbody_index_size
+        );
 
 
-        self.write_display_frames(&mut file);
+        self.write_display_frames(&mut file, bone_index_size, morph_index_size);
         file.write_u32::<LE>(0).unwrap(); // rigidbodys
         file.write_u32::<LE>(0).unwrap(); // joints
 
@@ -61,7 +112,7 @@ impl Pmx {
         }
     }
 
-    fn write_verts(&self, file: &mut Cursor<Vec<u8>>) {
+    fn write_verts(&self, file: &mut Cursor<Vec<u8>>, bone_index_size: u8) {
         file.write_u32::<LE>(self.verts.len() as _).unwrap();
         for v in &self.verts {
             write_float3(file, v.pos);
@@ -70,23 +121,26 @@ impl Pmx {
             match v.weight {
                 VertexWeight::One(b0) => {
                     file.write_u8(0).unwrap();
-                    file.write_i32::<LE>(b0).unwrap();
+                    Pmx::write_int(file, b0, bone_index_size);
                 },
                 VertexWeight::Two(b1, b2, w) => {
                     file.write_u8(1).unwrap();
-                    file.write_i32::<LE>(b1).unwrap();
-                    file.write_i32::<LE>(b2).unwrap();
+                    Pmx::write_int(file, b1, bone_index_size);
+                    Pmx::write_int(file, b2, bone_index_size);
                     file.write_f32::<LE>(w).unwrap();
                 },
                 VertexWeight::Four(bi, bw) => {
                     file.write_u8(2).unwrap();
-                    write_int4(file, bi);
+                    Pmx::write_int(file, bi[0], bone_index_size);
+                    Pmx::write_int(file, bi[1], bone_index_size);
+                    Pmx::write_int(file, bi[2], bone_index_size);
+                    Pmx::write_int(file, bi[3], bone_index_size);
                     write_float4(file, bw);
                 },
                 VertexWeight::Sphere(b1, b2, weight, c, r0, r1) => {
                     file.write_u8(3).unwrap();
-                    file.write_i32::<LE>(b1).unwrap();
-                    file.write_i32::<LE>(b2).unwrap();
+                    Pmx::write_int(file, b1, bone_index_size);
+                    Pmx::write_int(file, b2, bone_index_size);
                     file.write_f32::<LE>(weight).unwrap();
                     write_float3(file, c);
                     write_float3(file, r0);
@@ -94,7 +148,10 @@ impl Pmx {
                 },
                 VertexWeight::Quat(bi, bw) => {
                     file.write_u8(4).unwrap();
-                    write_int4(file, bi);
+                    Pmx::write_int(file, bi[0], bone_index_size);
+                    Pmx::write_int(file, bi[1], bone_index_size);
+                    Pmx::write_int(file, bi[2], bone_index_size);
+                    Pmx::write_int(file, bi[3], bone_index_size);
                     write_float4(file, bw);
                 },
             }
@@ -102,17 +159,25 @@ impl Pmx {
         }
     }
 
-    fn write_faces(&self, file: &mut Cursor<Vec<u8>>) {
+    fn write_faces(&self, file: &mut Cursor<Vec<u8>>, vertex_index_size: u8) {
         file.write_u32::<LE>(3 * self.faces.len() as u32).unwrap();
 
         for f in &self.faces {
-            file.write_u32::<LE>(f[0]).unwrap();
-            file.write_u32::<LE>(f[1]).unwrap();
-            file.write_u32::<LE>(f[2]).unwrap();
+            Pmx::write_uint(file, f[0] as _, vertex_index_size);
+            Pmx::write_uint(file, f[1] as _, vertex_index_size);
+            Pmx::write_uint(file, f[2] as _, vertex_index_size);
         }
     }
 
-    fn write_morphs(&self, file: &mut Cursor<Vec<u8>>)  {
+    fn write_morphs(
+        &self,
+        file: &mut Cursor<Vec<u8>>,
+        vertex_index_size: u8,
+        material_index_size: u8,
+        bone_index_size: u8,
+        morph_index_size: u8,
+        rigidbody_index_size: u8
+    )  {
         file.write_u32::<LE>(self.morphs.len() as _).unwrap();
         for morph in &self.morphs {
             Self::write_string(file, &morph.name);
@@ -123,28 +188,28 @@ impl Pmx {
                 Morph::MorphGroup(vs) => {
                     file.write_u32::<LE>(vs.len() as _).unwrap();
                     for v in vs {
-                        file.write_u32::<LE>(v.index).unwrap();
+                        Pmx::write_int(file, v.index as _, morph_index_size);
                         file.write_f32::<LE>(v.affect).unwrap();
                     }
                 },
                 Morph::MorphFlip(vs) => {
                     file.write_u32::<LE>(vs.len() as _).unwrap();
                     for v in vs {
-                        file.write_u32::<LE>(v.index).unwrap();
+                        Pmx::write_int(file, v.index as _, morph_index_size);
                         file.write_f32::<LE>(v.affect).unwrap();
                     }
                 },
                 Morph::MorphVertex(vs) => {
                     file.write_u32::<LE>(vs.len() as _).unwrap();
                     for v in vs {
-                        file.write_u32::<LE>(v.index).unwrap();
+                        Pmx::write_int(file, v.index as _, vertex_index_size);
                         write_float3(file, v.trans);
                     }
                 },
                 Morph::MorphBone(vs) => {
                     file.write_u32::<LE>(vs.len() as _).unwrap();
                     for v in vs {
-                        file.write_u32::<LE>(v.index).unwrap();
+                        Pmx::write_int(file, v.index as _, bone_index_size);
                         write_float3(file, v.trans);
                         write_quat(file, v.rot);
                     }
@@ -152,14 +217,14 @@ impl Pmx {
                 Morph::MorphUv(vs) => {
                     file.write_u32::<LE>(vs.len() as _).unwrap();
                     for v in vs {
-                        file.write_u32::<LE>(v.index).unwrap();
+                        Pmx::write_int(file, v.index as _, vertex_index_size);
                         write_float4(file, v.trans);
                     }
                 },
                 Morph::MorphRigidbody(vs) => {
                     file.write_u32::<LE>(vs.len() as _).unwrap();
                     for v in vs {
-                        file.write_u32::<LE>(v.index).unwrap();
+                        Pmx::write_int(file, v.index as _, rigidbody_index_size);
                         file.write_i8(if v.local { 1 } else {0}).unwrap();
                         write_float3(file, v.trans_speed);
                         write_float3(file, v.rot_torque);
@@ -168,7 +233,7 @@ impl Pmx {
                 Morph::MorphMat(vs) => {
                     file.write_u32::<LE>(vs.len() as _).unwrap();
                     for v in vs {
-                        file.write_u32::<LE>(v.index).unwrap();
+                        Pmx::write_int(file, v.index as _, morph_index_size);
                         match v.blend_mode {
                             BlendMode::Mul => {
                                 file.write_u8(0).unwrap();
@@ -194,7 +259,7 @@ impl Pmx {
         }
     }
 
-    fn write_mats(&self, file: &mut Cursor<Vec<u8>>) {
+    fn write_mats(&self, file: &mut Cursor<Vec<u8>>, texture_index_size: u8) {
         if self.faces.is_empty() {
             file.write_u32::<LE>(0).unwrap();
             return;
@@ -221,8 +286,8 @@ impl Pmx {
             file.write_u8(m.draw_flag.bits()).unwrap();
             write_float4(file, m.edge_color);
             file.write_f32::<LE>(m.edge_scale).unwrap();
-            file.write_i32::<LE>(m.tex_index).unwrap();
-            file.write_i32::<LE>(m.env_index).unwrap();
+            Pmx::write_int(file, m.tex_index, texture_index_size);
+            Pmx::write_int(file, m.env_index, texture_index_size);
             let env_blend_mode = match m.env_blend_mode {
                 BlendMode::Disable => 0,
                 BlendMode::Mul => 1,
@@ -233,7 +298,7 @@ impl Pmx {
             match m.toon {
                 Toon::Tex(i) => {
                     file.write_u8(0).unwrap();
-                    file.write_i32::<LE>(i).unwrap();
+                    Pmx::write_int(file, i, texture_index_size);
                 },
                 Toon::Inner(i) => {
                     file.write_u8(1).unwrap();
@@ -245,7 +310,7 @@ impl Pmx {
             file.write_u32::<LE>(m.associated_face_count * 3).unwrap();
         }
     }
-    fn write_bones(&self, file: &mut Cursor<Vec<u8>>) {
+    fn write_bones(&self, file: &mut Cursor<Vec<u8>>, bone_index_size: u8) {
         let default = vec![ Bone::default() ];
 
         let bones = if self.bones.is_empty() {
@@ -259,23 +324,23 @@ impl Pmx {
             Self::write_string(file, &b.name_en);
             write_float3(file, b.pos);
             if let Some(p) = b.parent_index {
-                file.write_i32::<LE>(p as _).unwrap();
+                Pmx::write_int(file, p as _, bone_index_size);
             } else {
-                file.write_i32::<LE>(-1).unwrap();
+                Pmx::write_int(file, -1, bone_index_size);
             };
             file.write_i32::<LE>(b.layer).unwrap();
 
             file.write_u16::<LE>(b.bone_flags.bits()).unwrap();
             match b.bone_tail_pos {
                 BoneTailPos::Bone(bi) => {
-                    file.write_i32::<LE>(bi).unwrap();
+                    Pmx::write_int(file, bi, bone_index_size);
                 },
                 BoneTailPos::Pos(pos) => {
                     write_float3(file, pos);
                 },
             }
             if let Some((parent_index, affect)) = b.inherit {
-                file.write_i32::<LE>(parent_index).unwrap();
+                Pmx::write_int(file, parent_index, bone_index_size);
                 file.write_f32::<LE>(affect).unwrap();
             }
             if let Some(v) = b.fixed_axis {
@@ -286,16 +351,16 @@ impl Pmx {
                 write_float3(file, y);
             }
             if let Some(external_parent) = b.external_parent {
-                file.write_i32::<LE>(external_parent).unwrap();
+                Pmx::write_int(file, external_parent, bone_index_size);
             }
             for ik in &self.iks {
                 if ik.bone == i as _ {
-                    file.write_i32::<LE>(ik.effector).unwrap();
+                    Pmx::write_int(file, ik.effector, bone_index_size);
                     file.write_i32::<LE>(ik.loop_count).unwrap();
                     file.write_f32::<LE>(ik.limit_angle).unwrap();
                     file.write_i32::<LE>(ik.ik_joints.len() as _).unwrap();
                     for j in &ik.ik_joints {
-                        file.write_i32::<LE>(j.bone).unwrap();
+                        Pmx::write_int(file, j.bone, bone_index_size);
                         if let Some((limit_min, limit_max)) = j.limit {
                             file.write_i8(1).unwrap();
                             write_float3(file, limit_min);
@@ -309,7 +374,7 @@ impl Pmx {
             }
         }
     }
-    fn write_display_frames(&self, file: &mut Cursor<Vec<u8>>) {
+    fn write_display_frames(&self, file: &mut Cursor<Vec<u8>>, bone_index_size: u8, morph_index_size: u8) {
         let display_frames: Vec<DisplayFrame> = if self.display_frames.is_empty() {
             vec![
                 DisplayFrame { name: "Root".to_string(), name_en: "Root".to_string(), deletable: true, morph_items: vec![DisplayFrameIndex::Bone(0)] },
@@ -329,12 +394,11 @@ impl Pmx {
                 match index {
                     DisplayFrameIndex::Bone(bi) => {
                         file.write_u8(0).unwrap();
-                        file.write_u32::<LE>(*bi).unwrap();
-                        
+                        Pmx::write_int(file, *bi as _, bone_index_size);
                     },
                     DisplayFrameIndex::Morph(mi) => {
                         file.write_u8(1).unwrap();
-                        file.write_u32::<LE>(*mi).unwrap();
+                        Pmx::write_int(file, *mi as _, morph_index_size);
                     },
                 }
             }
