5f93d38b26839cb23badba79a3aabd4b2eec599e
zhouhang95 <765229842@qq.com>
2023-12-29 01:59:30 +0800

    refactor

--- src/custom3d_wgpu.rs
+++ src/custom3d_wgpu.rs
@@ -149,29 +149,6 @@ impl Custom3d {
     }
 }
 
-impl eframe::App for Custom3d {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            egui::ScrollArea::both()
-                .auto_shrink(false)
-                .show(ui, |ui| {
-                    ui.horizontal(|ui| {
-                        ui.spacing_mut().item_spacing.x = 0.0;
-                        ui.label("The triangle is being painted using ");
-                        ui.hyperlink_to("WGPU", "https://wgpu.rs");
-                        ui.label(" (Portable Rust graphics API awesomeness)");
-                    });
-                    ui.label("It's not a very impressive demo, but it shows you can embed 3D inside of egui.");
-
-                    egui::Frame::canvas(ui.style()).show(ui, |ui| {
-                        self.custom_painting(ui);
-                    });
-                    ui.label("Drag to rotate!");
-                });
-        });
-    }
-}
-
 // Callbacks in egui_wgpu have 3 stages:
 // * prepare (per callback impl)
 // * finish_prepare (once)
